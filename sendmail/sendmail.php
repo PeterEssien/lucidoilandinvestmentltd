fcf15ca555661388ac1cab7060dcbe451a9a2bbd
Peter Essien <68255976+PeterEssien@users.noreply.github.com>
2022-11-21 11:20:05 +0100

    Update sendmail.php

--- sendmail/sendmail.php
+++ sendmail/sendmail.php
@@ -41,7 +41,7 @@ if(empty($_POST) === false) {
 				$email 		= test_input($_POST['email']);
 				$message 	= test_input($_POST['message']);
 
-				$to 		= "your-email@gmail.com";
+				$to 		= "essienpeter57@gmail.com";
 				$subject 	= "Your Website Name: You have a new message from " . $name;
 				
 				$body 		= "
