c38e41444af573dd1defc70cfde35d13c70952f2
Peter Essien <68255976+PeterEssien@users.noreply.github.com>
2022-11-21 10:57:14 +0100

    Update contact-form-process.php

--- contact-form-process.php
+++ contact-form-process.php
@@ -1,12 +1,20 @@
 <?php
-if (isset($_POST['email']) && !empty($_POST['email'])) {
-$subject = "New contact form request";
-$message = $_POST['message'];
-  $headers = 'From: info@website.com' . "\r\n" .
-             'Reply-To: ' . $_POST['email']. "\r\n" .
-             'X-Mailer: PHP/' . phpversion();
-
-  mail('essienpeter57@gmail.com', $subject, $message, $headers);
-
-  die('Thank you for your email');
+if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+  http_response_code(500);
+  exit();
 }
+
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email = strip_tags(htmlspecialchars($_POST['email']));
+$m_subject = strip_tags(htmlspecialchars($_POST['subject']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+
+$to = "golddyx@my.com"; // Change this email to your //
+$subject = "$m_subject:  $name";
+$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
+$header = "From: $email";
+$header .= "Reply-To: $email";	
+
+if(!mail($to, $subject, $body, $header))
+  http_response_code(500);
+?>
